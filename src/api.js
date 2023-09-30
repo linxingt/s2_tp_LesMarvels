8db6ddb2c7fe095e29a06fd03871a39f913416f8
Laurent Giustignano <laurent.giustignano@free.fr>
2023-09-30 18:33:30 +0200

    Début TP

---
+++ src/api.js
@@ -0,0 +1,23 @@
+
+
+/**
+ * Récupère les données de l'endpoint en utilisant les identifiants
+ * particuliers developer.marvels.com
+ * @param url l'end-point
+ * @return {Promise<json>}
+ */
+export const getData = async (url) => {
+    // A Compléter
+}
+
+/**
+ * Calcul la valeur md5 dans l'ordre : timestamp+privateKey+publicKey
+ * cf documentation developer.marvels.com
+ * @param publicKey
+ * @param privateKey
+ * @param timestamp
+ * @return {Promise<ArrayBuffer>} en hexadecimal
+ */
+export const getHash = async (publicKey, privateKey, timestamp) => {
+    // A compléter
+}
